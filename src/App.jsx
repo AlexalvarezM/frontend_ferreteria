d87db1c4165476cb9d0dbf191f9a2ae80e904741
AlexAlvarezM <01alexalvarez@gmail.com>
2025-06-10 18:44:35 -0600

    Rutas protegidas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Inicio from "./views/Inicio";
 import Encabezado from "./components/emcabezado/Encabezado";
+import RutaProtegida from "./components/rutas/RutasProtegida";
 
 import Productos from "./views/Productos";
 import Categorias from "./views/Categorias";
@@ -27,14 +28,14 @@ const App = () => {
             <Route path="/" element={<Login />} />
             <Route path="/inicio" element={<Inicio />} />
             
-            <Route path="/productos" element={<Productos />} />
-            <Route path="/categorias" element={<Categorias />} />
-            <Route path="/clientes" element={<Clientes />} />
-            <Route path="/ventas" element={<Ventas />} />
-            <Route path="/compras" element={<Compras />} />
-            <Route path="/usuarios" element={<Usuarios/>} />
-            <Route path="/empleados" element={<Empleados/>} />
-            <Route path="/estadisticas" element={<Estadisticas/>} />
+            <Route path="/productos" element={<RutaProtegida vista = {<Productos />} />} />
+            <Route path="/categorias" element={<RutaProtegida vista = {<Categorias />} />} />
+            <Route path="/clientes" element={<RutaProtegida vista = {<Clientes />} />} />
+            <Route path="/ventas" element={<RutaProtegida vista = {<Ventas />} />} />
+            <Route path="/compras" element={<RutaProtegida vista = {<Compras />} />} />
+            <Route path="/usuarios" element={<RutaProtegida vista = {<Usuarios/>} />} />
+            <Route path="/empleados" element={<RutaProtegida vista = {<Empleados/>} />} />
+            <Route path="/estadisticas" element={<RutaProtegida vista = {<Estadisticas/>} />} />
             <Route path="/dashboard" element={<Dashboard/>} />
 
 
